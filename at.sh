6e39864c98801f55766862b83c5b7cecb3f3ad3b
Keith Thomson <keith.a.thomson>
2019-07-05 22:24:24 +0100

    Simplify the at command.  Still wrong depending on BST/GMT

--- at.sh
+++ at.sh
@@ -1,13 +1,10 @@
 current=`date +"%s"`
 current_date=`date +'%Y-%m-%d'`
 future=`date -d "${current_date} $1" +"%s"`
-
 diff=$((future-current))
 if (( $diff < 0 ));
 then
-	tomorrow_date=`date +'%y-%m-%d'`
-	future=`TZ=$TZ+24 date -d "${current_date} $1" +"%s"`
-	diff=$((future-current))
+	diff=$(( (60*60*24) + $diff ))
 fi
 
 echo "Sleep for $diff seconds"
