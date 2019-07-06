1c31e0286b9d50b5346c9bb70f5fd155720f190a
Keith Thomson <keith.a.thomson>
2019-07-06 20:14:35 +0100

    Delete a bunch of older stuff.

--- download_all_new.sh
+++
@@ -1,9 +0,0 @@
-goes_meso.sh $1 $2 $3 1 16
-goes_meso.sh $1 $2 $3 2 16
-goes_meso.sh $1 $2 $3 1 17
-goes_meso.sh $1 $2 $3 2 17
-
-# Missing an hour 00->01
-#hima_japan.sh $1 $2 $3
-#hima_japan_ir.sh $1 $2 $3
-
--- myecho.sh
+++
@@ -1,1 +0,0 @@
-echo myecho $1 $2 $3
