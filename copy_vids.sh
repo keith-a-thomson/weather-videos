2d9cfa959164146183878bf60e9fbddfde140673
Keith Thomson <keith.a.thomson>
2019-10-31 13:01:28 +0000

    Update my personal copy command.

--- copy_vids.sh
+++ copy_vids.sh
@@ -1,4 +1,6 @@
-declare -a arr=("goes_16_conus" "goes_16_full" "hima_taiwan" "hima_full"
+declare -a arr=("goes_16_conus" "goes_16_full" 
+				"goes_17_conus" "goes_17_full"
+				"hima_taiwan" "hima_full"
                 "goes_16_meso_01" "goes_meso_01_ir"
 				"goes_16_meso_02" "goes_meso_02_ir"
 				"goes_17_meso_01"
@@ -8,5 +10,5 @@ declare -a arr=("goes_16_conus" "goes_16_full" "hima_taiwan" "hima_full"
 for i in "${arr[@]}"
 do
    echo "$i"
-   cp -p -u -v //keith-lapwing/keith/$i/20*.mp4 /cygdrive/e/vid/$i/
+   cp --preserve=timestamps -u -v /home/keith/$i/20*.mp4 //kestrel/e/vid/$i/
 done
