83dcde2d3137387d359e295b631dd1fefefc9ca6
Keith Thomson <keith.a.thomson>
2020-02-18 21:13:52 +0000

    Stop doing the IR downloads

--- download_all.sh
+++ download_all.sh
@@ -7,9 +7,9 @@ goes_conus.sh  $1 $2 $3 17
 hima_taiwan.sh $1 $2 $3
 hima_japan.sh  $1 $2 $3
 hima_meso.sh   $1 $2 $3
-irch13.sh      $1 $2 $3 1 goes
-irch13.sh      $1 $2 $3 2 goes
-irch13.sh      $1 $2 $3 1 hima
+#irch13.sh      $1 $2 $3 1 goes
+#irch13.sh      $1 $2 $3 2 goes
+#irch13.sh      $1 $2 $3 1 hima
 goes_meso.sh   $1 $2 $3 1 16
 goes_meso.sh   $1 $2 $3 2 16
 goes_meso.sh   $1 $2 $3 1 17
