3adf80eee2c3ee6708092311abe365d98677e7ff
Keith Thomson <keith.a.thomson>
2020-02-18 21:15:54 +0000

    Update the main script to avoid uploading in parallel. Also work out tomorrow date and auto download what is required.

--- process_upload_all2.sh
+++ process_upload_all2.sh
@@ -1,65 +1,68 @@
+download_all.sh $1 $2 $3
+
+date2=$1-$2-$3
+midnight=`date -d ${date2} +"%s"`
+tomorrow_t=$((midnight+86400+600))
+date_tomorrow=`date -d @${tomorrow_t} +'%Y %m %d'`
+download_all.sh ${date_tomorrow}
+
 hima_full.sh   $1 $2 $3
+
 goes_full.sh   $1 $2 $3 16 &
+
 process_main.sh $1 $2 $3 hima_full
-wait
-upload.sh hf $1 $2 $3 &
+upload.sh hf $1 $2 $3
 
+wait
 goes_full.sh   $1 $2 $3 17 &
+
 process_main.sh $1 $2 $3 goes_16_full
-wait
-upload.sh g16f $1 $2 $3 &
+upload.sh g16f $1 $2 $3
 
+wait
 goes_conus.sh  $1 $2 $3 16 &
+
 process_main.sh $1 $2 $3 goes_17_full
-wait
-upload.sh g17f $1 $2 $3 &
+upload.sh g17f $1 $2 $3
 
+wait
 goes_conus.sh  $1 $2 $3 17 &
+
 process_main.sh $1 $2 $3 goes_16_conus
-wait
-upload.sh g16c $1 $2 $3 &
+upload.sh g16c $1 $2 $3
 
+wait
 hima_taiwan.sh $1 $2 $3 &
+
 process_main.sh $1 $2 $3 goes_17_conus
-wait
-upload.sh g17c $1 $2 $3 &
+upload.sh g17c $1 $2 $3
 
-irch13.sh      $1 $2 $3 1 goes &
-process_main.sh $1 $2 $3 hima_taiwan
 wait
-upload.sh ht $1 $2 $3 &
-
-irch13.sh      $1 $2 $3 2 goes &
-process_main.sh $1 $2 $3 goes_meso_01_ir
-wait
-upload.sh gm1ir $1 $2 $3 &
-
-irch13.sh      $1 $2 $3 1 hima &
-process_main.sh $1 $2 $3 goes_meso_02_ir
-wait
-upload.sh gm2ir $1 $2 $3 &
-
 goes_meso.sh   $1 $2 $3 1 16 &
-process_main.sh $1 $2 $3 hima_meso_01_ir
-wait
-upload.sh hm1ir $1 $2 $3 &
 
+process_main.sh $1 $2 $3 hima_taiwan
+upload.sh ht $1 $2 $3
+
+wait
 goes_meso.sh   $1 $2 $3 2 16 &
+
 process_laters.sh $1 $2 $3 goes_16_meso_01
-wait
-upload.sh g16m1 $1 $2 $3 &
+upload.sh g16m1 $1 $2 $3
 
+wait
 goes_meso.sh   $1 $2 $3 1 17 &
-process_laters.sh $1 $2 $3 goes_16_meso_02
-wait
-upload.sh g16m2 $1 $2 $3 &
 
-goes_meso.sh   $1 $2 $3 2 17 &
-process_laters.sh $1 $2 $3 goes_17_meso_01
+process_laters.sh $1 $2 $3 goes_16_meso_02
+upload.sh g16m2 $1 $2 $3
+
 wait
-upload.sh g17m1 $1 $2 $3 &
+goes_meso.sh   $1 $2 $3 2 17 &
+
+process_laters.sh $1 $2 $3 goes_17_meso_01
+upload.sh g17m1 $1 $2 $3
+
+wait
 
 process_laters.sh $1 $2 $3 goes_17_meso_02
-wait
 upload.sh g17m2 $1 $2 $3
 
--- process_upload_early.sh
+++ process_upload_early.sh
@@ -1,8 +1,8 @@
 hima_japan.sh $1 $2 $3
 hima_meso.sh $1 $2 $3
+
 process_main.sh $1 $2 $3 hima_meso
-upload.sh hm $1 $2 $3 &
+upload.sh hm $1 $2 $3
 
 process_main.sh $1 $2 $3 hima_japan
-wait
 upload.sh hj $1 $2 $3
