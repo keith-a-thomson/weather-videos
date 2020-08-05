0fa682c6c136cc1ebe87499114c085ebc5ffce7d
Keith Thomson <keith.a.thomson>
2020-08-05 22:30:52 +0100

    Restore parallel uploading to the main script

--- process_upload_all2.sh
+++ process_upload_all2.sh
@@ -11,58 +11,50 @@ hima_full.sh   $1 $2 $3
 goes_full.sh   $1 $2 $3 16 &
 
 process_main.sh $1 $2 $3 hima_full
-upload.sh hf $1 $2 $3
-
 wait
+upload.sh hf $1 $2 $3 &
 goes_full.sh   $1 $2 $3 17 &
 
 process_main.sh $1 $2 $3 goes_16_full
-upload.sh g16f $1 $2 $3
-
 wait
+upload.sh g16f $1 $2 $3 &
 goes_conus.sh  $1 $2 $3 16 &
 
 process_main.sh $1 $2 $3 goes_17_full
-upload.sh g17f $1 $2 $3
-
 wait
+upload.sh g17f $1 $2 $3 &
 goes_conus.sh  $1 $2 $3 17 &
 
 process_main.sh $1 $2 $3 goes_16_conus
-upload.sh g16c $1 $2 $3
-
 wait
+upload.sh g16c $1 $2 $3 &
 hima_taiwan.sh $1 $2 $3 &
 
 process_main.sh $1 $2 $3 goes_17_conus
-upload.sh g17c $1 $2 $3
-
 wait
+upload.sh g17c $1 $2 $3 &
 goes_meso.sh   $1 $2 $3 1 16 &
 
 process_main.sh $1 $2 $3 hima_taiwan
-upload.sh ht $1 $2 $3
-
 wait
+upload.sh ht $1 $2 $3 &
 goes_meso.sh   $1 $2 $3 2 16 &
 
 process_laters.sh $1 $2 $3 goes_16_meso_01
-upload.sh g16m1 $1 $2 $3
-
 wait
+upload.sh g16m1 $1 $2 $3 &
 goes_meso.sh   $1 $2 $3 1 17 &
 
 process_laters.sh $1 $2 $3 goes_16_meso_02
-upload.sh g16m2 $1 $2 $3
-
 wait
+upload.sh g16m2 $1 $2 $3 &
 goes_meso.sh   $1 $2 $3 2 17 &
 
 process_laters.sh $1 $2 $3 goes_17_meso_01
-upload.sh g17m1 $1 $2 $3
-
 wait
+upload.sh g17m1 $1 $2 $3 &
 
 process_laters.sh $1 $2 $3 goes_17_meso_02
+wait
 upload.sh g17m2 $1 $2 $3
 
