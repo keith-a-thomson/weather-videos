9c45693ea8a3faf4e20243048d95daac8fdf895c
Keith Thomson <keith.a.thomson>
2020-08-06 22:12:33 +0100

    Fix issue with missing dir

--- taiwan_radar.sh
+++ taiwan_radar.sh
@@ -11,5 +11,6 @@ date1=$1$2$3
 date2=$1-$2-$3
 
 mkdir -p $date2/radar
+mkdir -p $date2/radar_extended_1000
 curl ${base_radar}CV1_TW_3600_${date1}[$4-$5][00-50:10].png -o ${date2}/radar/CV1_TW_3600_${date1}#1#2.png
 curl ${base_radar}CV1_1000_${date1}[$4-$5][00-50:10].png -o ${date2}/radar_extended_1000/CV1_1000_${date1}#1#2.png
