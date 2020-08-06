63bd36afcc80742a7cedf1725122ddfa1b357b5f
Keith Thomson <keith.a.thomson>
2020-08-06 09:41:24 +0100

    Fix capitalisation of the station variable name

--- taiwan_site_video.sh
+++ taiwan_site_video.sh
@@ -11,11 +11,11 @@ date_=$1-$2-$3
 
 cd ${HOME}/taiwan_radar/
 
-for station in CV1_RCLY_1000 CV1_RCNT_1000 CV1_RCSL_1000;
+for STATION in CV1_RCLY_1000 CV1_RCNT_1000 CV1_RCSL_1000;
 do
 	ffmpeg -hide_banner -f image2 \
 	       -pattern_type glob -i ${STATION}_${date}'*.png' \
 		   -vf "pad=1080:1080:40:40" \
 		   -vcodec libx265 -crf 10 -pix_fmt yuv420p \
-		   ${station}_${date}.mp4
+		   ${STATION}_${date}.mp4
 done
