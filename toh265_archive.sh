1d4702c419a0948b7d8fd0d1069106dcdb68c0fd
Keith Thomson <keith.a.thomson>
2021-02-02 14:54:33 +0000

    Use more compression

--- toh265_archive.sh
+++ toh265_archive.sh
@@ -1,4 +1,4 @@
 for file in "$@"
 do
-        ffmpeg.exe -y -i $file -c:v libx265 -crf 20 x265_crf20_$file
+        ffmpeg.exe -y -i $file -c:v libx265 -crf 28 x265_crf28_$file
 done
--- toh265_archive_60fps.sh
+++ toh265_archive_60fps.sh
@@ -1,4 +1,4 @@
 for file in "$@"
 do
-        ffmpeg.exe -y -i $file  -r 60 -filter:v "setpts=0.4*PTS" -c:v libx265 -crf 20 x265_crf20_$file
+        ffmpeg.exe -y -i $file  -r 60 -filter:v "setpts=0.4*PTS" -c:v libx265 -crf 28 x265_crf28_$file
 done
